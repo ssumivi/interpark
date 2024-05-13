07976707d0b542dc4c5f2a5db0a02c09bd81ff68
Lee <smmang@gmail.com>
2024-05-13 17:20:07 +0900

    모달창

--- js/main.js
+++ js/main.js
@@ -1,4 +1,28 @@
 window.onload = function () {
+  // 모달창 ==============================
+  const body = document.querySelector("body");
+  const modal = document.querySelector(".modal-wrap");
+  modal.addEventListener("click", function () {
+    modal.style.display = "none";
+    fadeout(modal);
+  });
+  //isOpen 값에 따라 스크롤을 제어하는 함수
+  function controlScroll(isOpen) {
+    if (isOpen) {
+      body.style.overflow = "hidden";
+    } else {
+      body.style.overflow = "auto";
+    }
+  }
+  // 초기 모달 상태 설정
+  const isOpen = true;
+  controlScroll(isOpen);
+  modal.addEventListener("click", function () {
+    modal.style.display = "none";
+    // 모달이 닫힐 때는 스크롤을 다시 활성화
+    controlScroll(false);
+  });
+  // ===========================================
   // <!-- promotion swiper -->
   var swiper = new Swiper(".sw-promotion", {
     slidesPerView: 1,
@@ -129,9 +153,9 @@ window.onload = function () {
   var swiper = new Swiper(".sw-books", {
     slidesPerView: 3,
     spaceBetween: 19,
-    grid:{
-      rows:3,
-      fill:"row"
+    grid: {
+      rows: 3,
+      fill: "row",
     },
     breakpoints: {
       1024: {
